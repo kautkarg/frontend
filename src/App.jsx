875067a7fbc1cd6585fe7a9a8373a3ebae8280bd
sarang <sarangkale66@gmail.com>
2025-01-21 16:04:36 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import { useRef, useState } from 'react'
-import './App.css'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
+import "./App.css";
+import NavBar from "./components/NavBar";
 
-function App() {
-  const [count, setCount] = useState(0)
-  const gsapRef = useRef();
-  useGSAP(()=>{
-    gsap.to(".box",{
-      x:1000,
-      duration:2,
-      delay:1
-    });
-    gsap.to(gsapRef.current,{
-      x:500,
-      rotate:360,
-      scale:3,
-      duration:2,
-      delay:1
-    },{scope:".container"});
-  })
-
-  return (
+function App(){
+  return(
     <>
-      <h1 className="box text-3xl font-bold underline">
-      Hello world!
-      </h1>
-      <div className="container">
-        <div ref={gsapRef} className="box w-5 h-5 bg-cyan-500">
-        </div>
-      </div>
+      <NavBar />
+      <Hero />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <div className="text-[red] font-extrabold text-3xl">
+      NavBar
+    </div>
+  );
+}
+
+export default NavBar;
