3752f068a2be3d8eb7a14e6893ce3bf23f900554
sarang <sarangkale66@gmail.com>
2025-01-30 10:49:51 +0530

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = forwardRef((props, ref) => {
   }, []); 
 
   return (
-    <div className="fixed w-screen px-10 md:px-0 z-[100000000]">
+    <div className="fixed w-screen px-10 md:px-0 z-[2]">
       <header className="flex flex-wrap justify-between items-center py-5 px-4 md:px-52">
         <h1 ref={headerRef} className="text-lg md:text-xl">Univens</h1>
         <button ref={buttonRef} className="bg-[#295AAD] text-white py-2 px-4 rounded-sm">
