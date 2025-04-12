fd9c1bbd9852263d8a650d2057e785704110e6cb
codersintu <skmahto9199@gmail.com>
2025-04-12 21:15:36 +0530

    after 1st rollback

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,9 +31,7 @@ const Header = forwardRef((props, ref) => {
     <div className="fixed w-screen px-10 md:px-0 z-[1000000]">
       <header className="flex flex-wrap justify-between items-center py-5 px-4 md:px-52">
         <h1 ref={headerRef} className="text-lg md:text-xl">Univens</h1>
-        <button ref={buttonRef} onClick={()=>{
-          navigate('/dashboard');
-        }} className="bg-[#295AAD] rounded-full text-white py-2 px-4">
+        <button ref={buttonRef} onClick={() => window.open("https://wa.me/9172725217", "_blank")} className="bg-[#295AAD] rounded-full text-white py-2 px-4">
           Contact Us
         </button>
       </header>
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import { persistQueryClient } from '@tanstack/react-query-persist-client';
 import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 import NotFound from './components/NotFound.jsx';
-import Dashboard from './components/Dashboard.jsx';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -34,7 +33,6 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
